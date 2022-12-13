9fd4c6dcc5e8068b2011d9cd692f72e838482a18
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-12-13 19:10:11 +1030

    Add files via upload

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
-int	ft_recursive_factorial_recursion(int i, int n)
-{
-	if (i == 0)
-	{
-		return (n);
-	}
-	if (i >=0)
-	{
-		return (ft_recursive_factorial_recursion(i - 1, n * i));
-	}
-	return (0);
-}
-
 int	ft_recursive_factorial(int nb)
 {
-	return (ft_recursive_factorial_recursion(nb, 1));
+	if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	if (nb < 0)
+	{
+		return (0);
+	}
+	return (nb *= ft_recursive_factorial(nb - 1));
 }
 /*
 int	main()
 {
-	int	i;
-
-	i = 12;
-	printf("%d\n", ft_recursive_factorial(i));
-	return(0);
+	printf("%d\n", ft_recursive_factorial(5));
+	return (0);
 }
 */
