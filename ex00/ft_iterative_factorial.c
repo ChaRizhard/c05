916365f03fabf74d841d4563d76a5aa0109a62c6
ChaRizhard <66208087+ChaRizhard@users.noreply.github.com>
2022-12-18 15:08:44 +1030

    Add files via upload

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 12:40:22 by rtran             #+#    #+#             */
+/*   Updated: 2022/12/15 15:07:52 by rtran            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 int	ft_iterative_factorial(int nb)
@@ -9,13 +21,13 @@ int	ft_iterative_factorial(int nb)
 	n = 1;
 	if (nb < 0)
 	{
-		return(0);
+		return (0);
 	}
 	while (++i <= nb)
 	{
 		n *= i;
 	}
-	return(n);
+	return (n);
 }
 /*
 int	main()
